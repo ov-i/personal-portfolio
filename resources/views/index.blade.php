05bfa7dcdb5f1f96d4970ff7a63a6723e6f88fb9
Bartosz Pazdur <b.pazdur@onet.pl>
2022-01-25 19:05:22 +0100

    New layout and content
    Rename welcome -> index. And fix routing

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+@section('content')
+    <h1 class="text-primary-accent font-body"><% msg %></h1>
+    <p class="text-gray font-display font-light">
+        Lorem ipsum dolor sit a`m`et consectetur adipisicing elit. Laboriosam, reprehenderit!
+    </p>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bartosz Pazdur | @yield('title')</title>
+
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&family=Roboto+Condensed&display=swap" rel="stylesheet">
+
+    <!-- custom styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<div id="app" class="app" v-cloak>
+
+</div>
+
+<!-- custom scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
