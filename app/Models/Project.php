f07f9025c916f0a1f7329f73611219ae7d34f59b
Bartosz Pazdur <b.pazdur@onet.pl>
2022-01-26 13:55:25 +0100

    New model content
    Add new fillable contents

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'project_name',
+        'language',
+        'repository_url',
+        'description',
+        'website_url',
+        'project_status'
+    ];
 }
