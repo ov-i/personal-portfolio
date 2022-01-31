8483e64d71c80cfdb8d277e3ade0caf01bbc5efa
Bartosz Pazdur <b.pazdur@onet.pl>
2022-01-31 16:30:03 +0100

    new content
    Add new content for front page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta name="description" content="Cześć! Jestem Bartosz Pazdur. Na codzień pracuję oraz rozwijam się na
+    stanowisku full stack web developera. Moją główną specjalizacja jest JavaScript w połączeniu z środowiskiem
+    uruchomieniowym NodeJS. W warstwie wizualnej pracują z wykorzystaniem frameworka Vue w wersji 3 bądź drugiej,
+    w zależnośći od specyfikacji projektu. Chcesz wiedzieć więcej? Zapraszam na https://github.com/BrittleHeart">
+    <meta name="robots" content="index, follow">
+    <meta name="keywords" content="portfolio, bartosz, pazdur, overmind, brittle heart, bartosz pazdur, github
+    Vue, Laravel, SEO, full stack, full, stack, zzmwruda, zzmwruda.pl, overblitz, web, developer, bartoszpazdur.pl,
+    staysecure, facebook, #staysecure">
+
+    <meta name="language" content="pl-pl">
+
+    <meta name="og:title" content="Bartosz Pazdur Portfolio">
+    <meta name="og:description" content="Cześć! Jestem Bartosz Pazdur. Na codzień pracuję oraz rozwijam się na
+    stanowisku full stack web developera. Moją główną specjalizacja jest JavaScript w połączeniu z środowiskiem
+    uruchomieniowym NodeJS.">
+    <meta name="og:image" content="{{ asset('img/background.jpg') }}">
+
     <title>Bartosz Pazdur | @yield('title')</title>
 
     <!-- Google fonts -->
@@ -16,34 +34,293 @@
 </head>
 <body>
 <div id="app" class="app" v-cloak role="application">
-    {{-- Main header of application --}}
-    <header
-        class="app-header bg-hero-background h-screen bg-fixed bg-center material-padding"
-        aria-label="Treść nagłówkowa strony"
-        role="presentation">
-
-        {{-- Main navigation of application --}}
-        <nav class="app-nav flex justify-between items-center" role="navigation">
-            <section class="nav-brand" aria-label="Nazwa strony" role="region">
-                <a class="font-primary font-medium text-white text-xl decoration-0 tracking-normal">
-                    Bartosz Pazdur
-                </a>
-            </section>
-            {{-- navigation items --}}
-            <section class="navigation-list" role="navigation">
-                <ul role="list" class="list-none">
-                    <li role="listitem" class="inline-block font-primary font-regular text-white pr-3">start</li>
-                    <li role="listitem" class="inline-block font-primary font-regular text-white pr-3">o mnie</li>
-                    <li role="listitem" class="inline-block font-primary font-regular text-white pr-3">projekty</li>
-                    <li role="listitem" class="inline-block font-primary font-regular text-white">
-                        <a href="{{ route('home.index') }}">Blog</a>
+    <header aria-label="Baner aplikacji" role="banner" class="font-primary" id="primary-heading">
+        <nav class="px-3" id="primary-navigation" aria-label="Nawigacja" role="navigation">
+            <div class="font-medium">
+                <a href="#" class="text-2xl " role="link">Bartosz Pazdur</a>
+            </div>
+            <div>
+                <span class="iconify" data-icon="clarity:bars-line"></span>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <a href="#">start</a>
+                    </li>
+                    <li>
+                        <a href="#">o mnie</a>
+                    </li>
+                    <li>
+                        <a href="#">projekty</a>
+                    </li>
+                    <li>
+                        <a href="#">blog</a>
                     </li>
                 </ul>
-            </section>
+            </div>
         </nav>
+        <section>
+            <article>
+                <p>Bartosz Pazdur</p>
+                <h1>Web Developer</h1>
+                <span>"admin, think, improve."</span>
+            </article>
+        </section>
     </header>
+    <main class="" id="" aria-label="Główna treść" role="main">
+        <!-- section about me -->
+        <section class="font-primary">
+            <article>
+                <h2>O mnie</h2>
+                <span>Poznaj mnie</span>
+                <div class="divider w-8 p-1 bg-accent"></div>
+            </article>
+            <section>
+                <article>
+                    <h2 class="font-medium">
+                        Jestem
+                        <span class="text-accent font-semibold">Bartosz Pazdur</span>,
+                        Web Developer
+                    </h2>
+                    <p class="font-normal">
+                        Na co dzień tworzę, oraz rozwijam nowe projekty z wykorzystaniem nowoczesnych technologii.
+                        Jestem również pracownikiem w przedsiębiorstwie Encode IT Rafał Jaseniuk na stanowisku Full Stack Web Developera.
+                    </p>
+                    <p class="font-normal">
+                        Poza pracą jako programista, interesuję się elektroniką w odniesieniu do budowy robotów
+                        z wykorzystaniem Arduino UNO, zabawą z RasberryPi i tym podobnych.
+                    </p>
+                </article>
+                <article>
+                    <div class="info">
+                        <div class="single-info">
+                            <p class="font-medium">
+                                Imię i nazwisko:
+                                <span class="font-normal">Bartosz Pazdur</span>
+                            </p>
+                        </div>
+                        <div class="single-info">
+                            <p class="font-medium">
+                                Email:
+                                <span class="text-accent">b.pazdur@yahoo.com</span>
+                            </p>
+                        </div>
+                        <div class="single-info">
+                            <p class="font-medium">
+                                Wiek:
+                                <span class="font-normal">{{ date('Y') - 1998 }}</span>
+                            </p>
+                        </div>
+                        <div class="single-info">
+                            <p class="font-medium">
+                                Krja
+                                <span class="font-normal">Polska</span>
+                            </p>
+                        </div>
+                        <div class="single-info">
+                            <button>Ściągnij CV</button>
+                        </div>
+                    </div>
+                </article>
+            </section>
+        </section>
 
-    @yield('content')
+        <!-- section projects -->
+        <section>
+            <article>
+                <h2 class="uppercase">projekty</h2>
+                <span>Czym się zajmuję?</span>
+                <div class="divider w-8 p-1 bg-accent"></div>
+            </article>
+            <!-- projects -->
+            <article class="projects">
+                <!-- single project -->
+                <div class="project">
+                    <span>javascipt</span>
+                    <div class="project-info">
+                        <div class="project-title font-primary">
+                            <h3 class="heading">Projekt CRM</h3>
+                        </div>
+                        <div class="project-socials">
+                            <div class="social">
+                                <span class="iconify" data-icon="akar-icons:github-fill"></span>
+                            </div>
+                            <div class="social">
+                                <span class="iconify" data-icon="whh:website"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="project-description">
+                        <p>
+                            Projekt koncentrujący się na stworzeniu systemu CRM do
+                            użytku własnego w celu prowadzenia kursów z zakresu programowania.
+                        </p>
+                    </div>
+                    <a href="#">Zobacz więcej</a>
+                </div>
+                <!-- single project -->
+                <div class="project">
+                    <span>javascipt</span>
+                    <div class="project-info">
+                        <div class="project-title font-primary">
+                            <h3 class="heading">Projekt CRM</h3>
+                        </div>
+                        <div class="project-socials">
+                            <div class="social">
+                                <span class="iconify" data-icon="akar-icons:github-fill"></span>
+                            </div>
+                            <div class="social">
+                                <span class="iconify" data-icon="whh:website"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="project-description">
+                        <p>
+                            Projekt koncentrujący się na stworzeniu systemu CRM do
+                            użytku własnego w celu prowadzenia kursów z zakresu programowania.
+                        </p>
+                    </div>
+                    <a href="#">Zobacz więcej</a>
+                </div>
+                <!-- single project -->
+                <div class="project">
+                    <span>javascipt</span>
+                    <div class="project-info">
+                        <div class="project-title font-primary">
+                            <h3 class="heading">Projekt CRM</h3>
+                        </div>
+                        <div class="project-socials">
+                            <div class="social">
+                                <span class="iconify" data-icon="akar-icons:github-fill"></span>
+                            </div>
+                            <div class="social">
+                                <span class="iconify" data-icon="whh:website"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="project-description">
+                        <p>
+                            Projekt koncentrujący się na stworzeniu systemu CRM do
+                            użytku własnego w celu prowadzenia kursów z zakresu programowania.
+                        </p>
+                    </div>
+                    <a href="#">Zobacz więcej</a>
+                </div>
+                <!-- single project -->
+                <div class="project">
+                    <span>javascipt</span>
+                    <div class="project-info">
+                        <div class="project-title font-primary">
+                            <h3 class="heading">Projekt CRM</h3>
+                        </div>
+                        <div class="project-socials">
+                            <div class="social">
+                                <span class="iconify" data-icon="akar-icons:github-fill"></span>
+                            </div>
+                            <div class="social">
+                                <span class="iconify" data-icon="whh:website"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="project-description">
+                        <p>
+                            Projekt koncentrujący się na stworzeniu systemu CRM do
+                            użytku własnego w celu prowadzenia kursów z zakresu programowania.
+                        </p>
+                    </div>
+                    <a href="#">Zobacz więcej</a>
+                </div>
+            </article>
+        </section>
+
+        <!-- section blog -->
+        <section>
+            <article>
+                <h2 class="uppercase">mój blog</h2>
+                <span>Wpisy z bloga</span>
+                <div class="divider w-8 p-1 bg-accent"></div>
+            </article>
+            <!-- posts -->
+            <article class="posts">
+                <!-- single post -->
+                <div class="post">
+                    <span class="uppercase font-primary font-semibold">
+                        Bezpieczeństwo
+                    </span>
+                </div>
+                <!-- single post -->
+                <div class="post">
+                    <span class="uppercase font-primary font-semibold">
+                        Programowanie
+                    </span>
+                </div>
+                <!-- single post -->
+                <div class="post">
+                    <span class="uppercase font-primary font-semibold">
+                        ITTalks
+                    </span>
+                </div>
+                <!-- single post -->
+                <div class="post">
+                    <span class="uppercase font-primary font-semibold">
+                        kursy
+                    </span>
+                </div>
+            </article>
+        </section>
+    </main>
+
+    <footer class="" id="">
+        <!-- footer primary section -->
+        <section class="footer-primary">
+            <!-- single footer widget -->
+            <div class="widget footer-info">
+                <h3>Mapa strony</h3>
+                <div class="footer-list">
+                    <ul>
+                        <li>
+                            <a href="#">start</a>
+                        </li>
+                        <li>
+                            <a href="#">o mnie</a>
+                        </li>
+                        <li>
+                            <a href="#">projekty</a>
+                        </li>
+                        <li>
+                            <a href="#">blog</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="widget footer-info">
+                <h3>Ostatnie posty</h3>
+                <div class="footer-list">
+                    <ul>
+                        <li>Jak działają hasła maskowane?</li>
+                        <li>Manadżery haseł</li>
+                        <li>Uwierzytelnianie dwuskładnikowe</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="widget footer-info">
+                <h3>Dostępne kategorie</h3>
+                <div class="footer-list">
+                    <ul>
+                        <li>Bezpieczeństwo</li>
+                        <li>Programowanie</li>
+                        <li>ITTalks</li>
+                        <li>kursy</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section class="footer-secondary">
+            <div class="copyright">
+                Bartosz Pazdur &copy; {{ date('Y') }}
+            </div>
+        </section>
+    </footer>
 </div>
 
 <!-- custom scripts -->
