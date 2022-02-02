00d8bcde21848ac4a5bddbbdd8ec1a07df1694d4
Bartosz Pazdur <b.pazdur@onet.pl>
2022-02-02 11:41:20 +0100

    New component
    Add new Vue component for reduce code complexity

---
+++ resources/js/components/SectionHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <article aria-label="about me heading" id="about-me-heading">
+        <h2 class="relative text-center z-10 font-semibold">
+            <span class="opacity-20 uppercase text-dark-100 md:text-9xl text-7xl">{{ header }}</span>
+            <span class="absolute capitalize z-20 text-dark-700 top-1/2 left-1/2 md:text-4xl text-3xl transform -translate-x-1/2 -translate-y-1/2 opacity-90 font-semibold">
+                {{ title }}
+                <div class="divider w-1/4 p-0.5 mt-2 bg-accent mx-auto"></div>
+            </span>
+        </h2>
+    </article>
+</template>
+
+<script>
+export default {
+    name: "SectionHeader",
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        header: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
