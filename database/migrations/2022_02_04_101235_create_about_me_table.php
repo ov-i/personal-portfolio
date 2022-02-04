f5cdfb6976380c9269fb8753f068d59dfced5fa8
Bartosz Pazdur <b.pazdur@onet.pl>
2022-02-04 11:18:10 +0100

    New migrations and models
    Add new migrations and models for aboutme section

---
+++ database/migrations/2022_02_04_101235_create_about_me_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAboutMeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('about_me', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('about_work', 200);
+            $table->string('about_interest', 200);
+            $table->string('email');
+            $table->string('cv_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('about_mes');
+    }
+}
