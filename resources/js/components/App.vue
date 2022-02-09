f4dc6f19911ec8aa362ed5624332b92a4dc74d89
Bartosz <b.pazdur@yahoo.com>
2022-02-09 18:27:05 +0100

    New functionality
    Add Vue templates for admin page

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,10 +1,27 @@
 <template>
-    <h1>Works</h1>
+    <section class="admin-panel">
+        <nav class="admin-nav fixed mb-20 p-4 bg-white shadow-md w-full" role="navigation" aria-label="navigation">
+            <div class="nav-toggler">
+                <Icon icon="la:bars" class="iconify text-4xl z-20" data-icon="la:bars"></Icon>
+            </div>
+        </nav>
+        <div class="admin-panel-wrapper flex items-center">
+            <div class="navigation-panel w-full md:w-4/12 bg-blog-accent h-screen">
+
+            </div>
+            <div class="administration-panel"></div>
+        </div>
+    </section>
 </template>
 
 <script>
+import { Icon } from '@iconify/vue2'
+
 export default {
-    name: "App.vue"
+    name: "App.vue",
+    components: {
+        Icon,
+    }
 }
 </script>
 
