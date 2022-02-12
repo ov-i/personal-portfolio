5fde425e70a1ee6a58430019f8492288ae32226a
Bartosz Pazdur <b.pazdur@onet.pl>
2022-02-12 16:19:44 +0100

    Code readability
    Fix code readability

--- resources/js/Application.vue
+++ resources/js/Application.vue
@@ -3,7 +3,7 @@
         <div class="admin-panel-wrapper flex items-start relative">
             <div class="navigation-panel w-full retina:w-1/12 md:w-3/12 bg-blog-accent h-screen mr-4 absolute left-0 top-0 md:relative hidden md:block">
                 <div class="actions flex flex-col items-center relative">
-                    <SidebarItemWrapper />
+                    <sidebar-item-wrapper />
                 </div>
             </div>
             <div class="administration-panel w-full">
--- resources/js/components/SidebarItems/SidebarItemWrapper.vue
+++ resources/js/components/SidebarItems/SidebarItemWrapper.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="actions">
-        <SidebarItem />
+        <sidebar-item />
     </div>
 </template>
 
