c4f4330434122033d95af0874e4ae9c6239bb471
Bartosz Pazdur <b.pazdur@onet.pl>
2022-02-14 12:47:06 +0100

    New annotations
    Add new annotations for actions and mutations

--- resources/js/store/actions.js
+++ resources/js/store/actions.js
@@ -1,3 +1,17 @@
+/**
+ * Sets fetch error message
+ * @param commit
+ * @param error
+ */
 export const handleFetchError = ({ commit }, error) => {
     commit('HANDLE_FETCH_ERROR', error)
+}
+
+/**
+ * Sets request url state
+ * @param commit
+ * @param requestUrl
+ */
+export const setRequestUrl = ({ commit }, requestUrl) => {
+    commit('SET_REQUEST_URl', requestUrl)
 }
--- resources/js/store/mutations.js
+++ resources/js/store/mutations.js
@@ -1,3 +1,19 @@
+/**
+ * Mutates fetch errors state
+ * @param state
+ * @param error
+ * @constructor
+ */
 export const HANDLE_FETCH_ERROR = (state, error) => {
     state.fetchErrors.push(error);
+}
+
+/**
+ * Sets request url object state
+ * @param state
+ * @param requestUrl
+ * @constructor
+ */
+export const SET_REQUEST_URl = (state, requestUrl) => {
+    state.requestUrl = requestUrl
 }
