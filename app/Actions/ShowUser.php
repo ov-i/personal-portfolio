b9e8c048292552439eb919a44d2bdf681ed44190
Bartosz Pazdur <b.pazdur@onet.pl>
2022-02-14 17:18:35 +0100

    New method
    Add new show method for showing user

---
+++ app/Actions/ShowUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\User;
+use Symfony\Component\CssSelector\Exception\InternalErrorException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ShowUser
+{
+    public function __invoke(int $id): User
+    {
+        $user = null;
+
+        try {
+            $user = User::find($id);
+        } catch (NotFoundHttpException $notfound) {
+            response($notfound->getMessage(), 404);
+        } catch (InternalErrorException $internalErrorException) {
+            response($internalErrorException->getMessage(), 500);
+        }
+
+        return $user;
+    }
+}
