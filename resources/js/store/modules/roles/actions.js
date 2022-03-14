160ac30c4a6b76598ab1874eca390be14105c7ea
Bartosz Pazdur <b.pazdur@onet.pl>
2022-03-14 10:08:12 +0100

    New functionality
    Add new functionality for fetching roles

--- resources/js/store/modules/roles/actions.js
+++ resources/js/store/modules/roles/actions.js
@@ -7,6 +7,28 @@ import axios from 'axios'
  * @param dispatch
  * @param getters
  */
-export const fetchRoles = ({ commit, dispatch, getters }) => {
+export const fetchRoles = async ({ commit, dispatch, getters }) => {
+    /**
+     * selects endpoint for application
+     *
+     * @type {string}
+     */
+    const endpoint = `${getters.getRequestUrl}/roles`
 
+    try {
+        /**
+         * gets axios response for getting roles
+         *
+         * @type {AxiosResponse<any>}
+         */
+        const roles = await axios.get(endpoint, {
+            headers: {
+                'Content-Type': "application/json"
+            }
+        })
+
+        commit('FETCH_ROLES', roles.data)
+    } catch (error) {
+        dispatch('notFoundException', 'Problem z przetworzeniem danych')
+    }
 }
