c2e7f1257e5f0f4ae35f105dde9b8014df70cc82
Bartosz <b.pazdur@yahoo.com>
2022-03-21 15:03:09 +0100

    New functionality
    Add new functionality for authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthRequest;
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use phpDocumentor\Reflection\Types\Collection;
+
+class AuthController extends Controller
+{
+    /**
+     * return view for auth
+     *
+     * @return Application|Factory|View|RedirectResponse
+     */
+    public function login(): View|Factory|RedirectResponse|Application
+    {
+        if (auth()->check() && $this->isAdmin())
+            return redirect()->route('admin.home');
+        elseif(auth()->check() && !$this->isAdmin())
+            return redirect()->route('user.home');
+
+        return view('auth.login');
+    }
+
+    /**
+     * authenticates user
+     *
+     * @param AuthRequest $request
+     * @return RedirectResponse
+     */
+    public function authenticate(AuthRequest $request): RedirectResponse
+    {
+        $email = htmlentities($request['email']);
+        $password = htmlentities($request['password']);
+        $remember = $request->boolean('remember');
+
+        $user = User::where('email', '=', $email)->first();
+        if (!$user)
+            return redirect()->route('auth.login')
+                ->with('notFound', "Nie można odanelźć użytkownika o adresie {$email}");
+
+        $logged_in = auth()->attempt(['email' => $email, 'password' => $password], $remember);
+        if (!$logged_in)
+            return redirect()->route('auth.login')
+                ->with('badCredentials', 'Nieprawidłowe dane logowania');
+
+        if (!$this->isAdmin())
+            return redirect()->route('user.home')->with('user', auth()->user());
+
+        return redirect()->route('admin.home');
+    }
+
+    /**
+     * logouts the user
+     *
+     * @return RedirectResponse
+     */
+    public function logout(): RedirectResponse
+    {
+        auth()->logout();
+        return redirect()->route('auth.login');
+    }
+
+    /**
+     * gets logged in user roles
+     *
+     * @return null | array
+     */
+    public function getUserRole(): ?array
+    {
+        if (!auth()->check())
+            return null;
+
+        return auth()->user()->roles()->get()->toArray();
+    }
+
+    /**
+     * checks if user is admin
+     *
+     * @return bool|null
+     */
+    public function isAdmin(): ?bool
+    {
+        if (!auth()->check())
+            return null;
+
+        $isAdmin = false;
+        foreach ($this->getUserRole() as $key => $roles) {
+            if (isset($roles['name']) && $roles['name'] === 'Administrator')
+                $isAdmin = true;
+        }
+
+        return $isAdmin;
+    }
+}
