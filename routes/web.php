086a8b653924c480d149c949948335f83177f70c
Bartosz <b.pazdur@yahoo.com>
2022-03-21 15:01:24 +0100

    New routes solution
    Add new routes for api, users and admin

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,12 @@ use App\Http\Controllers\API\Admin\CategoriesController;
 use App\Http\Controllers\API\Admin\PostsController;
 use App\Http\Controllers\API\Admin\RolesController;
 use App\Http\Controllers\API\Admin\UsersController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\PostCategoryController;
 use App\Http\Controllers\SearchController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,24 +24,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// home actions
 Route::get('/', [HomePageController::class, 'index'])->name('home.index');
 Route::get('/download_cv', [HomePageController::class, 'download'])->name('home.download');
 
+// auth actions
+Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('/auth/authenticate', [AuthController::class, 'authenticate'])->name('auth.authenticate');
+Route::get('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
 // Blog section
-Route::get("/blog", [BlogController::class, 'index'])
-    ->name('blog.index');
-
+Route::get("/blog", [BlogController::class, 'index'])->name('blog.index');
 Route::post('/blog/search', [SearchController::class, '__invoke'])->name('blog.search');
-
 Route::get('/blog/wpisy/{post:slug}', [BlogController::class, 'show'])->name('blog.show');
 Route::get('/blog/kategoria/{category:name}/wpisy', [BlogController::class, 'categories'])
     ->name('blog.categories');
 
 Route::get('/blog/tags/{tag:name}/posts', [BlogController::class, 'tags'])->name('blog.tags');
 
+// routes protected by auth
 Route::middleware('auth')->group(function () {
+    // administration panel
     Route::controller(AdminController::class)->prefix('admin')->group(function () {
-        Route::get('/', 'index');
+        Route::get('/', 'index')->name('admin.home');
         Route::get('/{wildcard}', 'index');
         Route::get('/{wildcard}/{current}', 'index');
         Route::get('/{wildcard}/{current}/edit', 'index');
@@ -47,7 +54,20 @@ Route::middleware('auth')->group(function () {
         Route::get('/{wildcard}/{current}/preview', 'index');
     });
 
+    // user panel
+    Route::controller(UserController::class)->prefix('panel')->group(function () {
+        Route::get('/', fn() => 'Panel użytkownika')->name('user.home');
+    });
+
+    // api calls
     Route::prefix('api')->group(function () {
+        Route::get('/authData', function () {
+            if (!auth()->check())
+                return response()->json(['error' => true, 'data' => []], 400);
+
+            $data = auth()->user();
+            return response()->json(['error' => false, 'data' => $data]);
+        });
         Route::apiResource('/users', UsersController::class);
         Route::apiResource('/posts', PostsController::class);
         Route::put('/post/{post}/publish', [PostsController::class, 'publishPost']);
