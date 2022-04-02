e8c63a66036621dd8dd26e9102487ba65944b7f0
Bartosz <b.pazdur@yahoo.com>
2022-04-02 12:41:00 +0200

    New functionality
    Add new functionality for user register

--- resources/js/store/modules/comments/mutations.js
+++ resources/js/store/modules/comments/mutations.js
@@ -1,1 +1,13 @@
-// code ..
+/**
+ *
+ * @param state
+ * @param comments {array<{
+     * author: string,
+     * post_id: number,
+     * comment: string,
+     * published: boolean,
+     * created_at: Date
+ * }>}
+ * @constructor
+ */
+export const FETCH_COMMENTS = (state, comments) => state.comments = comments
