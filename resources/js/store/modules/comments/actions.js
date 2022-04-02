ca481e2fd3e2b4835843f735c87d9b64d11ebf6a
Bartosz <b.pazdur@yahoo.com>
2022-04-02 12:47:35 +0200

    New action
    Add new action for fetching posts

--- resources/js/store/modules/comments/actions.js
+++ resources/js/store/modules/comments/actions.js
@@ -1,1 +1,30 @@
 import axios from 'axios'
+
+/**
+ * fetches comments
+ *
+ * @param commit
+ * @param dispatch
+ * @param getters
+ * @return void
+ */
+export const fetchComments = async ({ commit, dispatch, getters }) => {
+    /**
+     * gets endpoint for api
+     *
+     * @type {string}
+     */
+    const endpoint = `${getters.getRequestUrl}/comments`
+
+    try {
+        const { data } = await axios.get(endpoint, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        commit('FETCH_COMMENTS', data.comments)
+    } catch (error) {
+        dispatch('notFoundException', 'Problem z przetworzeniem danych')
+    }
+}
